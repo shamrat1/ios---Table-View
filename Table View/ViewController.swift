f08e991498d915fd53bbcbad773f2c74a32d630a
Yasin Shamrat <yshamrat@gmail.com>
2019-12-10 12:14:05 +0600

    Table View New row addition and deletion, Now working

--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -7,8 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var allTime = ["Bun","Pie Cake","Choco cake","Toast","Biscuit"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var productName: UITextField!
+ 
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTime.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "All Time \(allTime[indexPath.row])"
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            allTime.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    @IBAction func addButton(_ sender: UIButton) {
+        allTime.append(productName.text!)
+        productName.text = nil
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
