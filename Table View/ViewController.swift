3754664ea0bae54ab9e0796e26cc0026e9e613ba
Yasin Shamrat <yshamrat@gmail.com>
2019-12-10 10:27:44 +0600

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Leads Computer on 12/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
